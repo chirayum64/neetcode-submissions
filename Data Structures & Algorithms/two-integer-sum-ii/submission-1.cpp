8fd16b7f9eccf361b7e2361aa7649e13d2843263
CHIRAYU M <pes1ug23am089@pesu.pes.edu>
2026-08-31 16:08:35 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<numbers.size();i++){
+            int req=target-numbers[i];
+            if(mp.find(req)!=mp.end()){
+                  return {mp[req]+1,i+1};
+            }
+            else{
+                mp[numbers[i]]=i;
+            }
+        }
+    return {-1,-1};
+    }
+};
